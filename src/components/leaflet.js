93c35653a5b895156d11f3339cdcadfa1d736250
Gunnar Magholder <gmagholder@gmail.com>
2018-10-29 19:23:39 +0100

    working on the leaflet stuff without any framework support

--- src/components/leaflet.js
+++ src/components/leaflet.js
@@ -2,23 +2,9 @@ import React, { Component } from 'react';
 
 
 export default class Map extends Component {
-    constructor(props) {
-        super(props);
-    }
+
     componentDidMount() {
-           // console.log("Loading map into #mymap...");
-            var map = new Microsoft.Maps.Map('#myMap', {});
-            //Request the user's location
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var loc = new Microsoft.Maps.Location(
-                    position.coords.latitude,
-                    position.coords.longitude);
-                //Add a pushpin at the user's location.
-                var pin = new Microsoft.Maps.Pushpin(loc);
-                map.entities.push(pin);
-                //Center the map on the user's location.
-                map.setView({ center: loc, zoom: 15 });
-                });
+
             }
     render() {
         return(
