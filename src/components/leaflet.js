1e5681b915dbe95073a896ff776d4e914e4caab4
Gunnar Magholder <gmagholder@gmail.com>
2018-10-29 12:43:51 +0100

    Getting rid of bing maps and google maps

--- src/components/leaflet.js
+++ src/components/leaflet.js
@@ -5,7 +5,7 @@ export default class Map extends Component {
     render() {
         return(
             <div>
-                <p>hello map</p>
+
             </div>
         );
     }
--- src/components/leaflet.test.js
+++ src/components/leaflet.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Map from './googlemap';
+import Map from './bingmap';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
